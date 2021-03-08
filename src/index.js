a11682dfaff88801a05bb272dec9c6f023bd16e7
BANDI PRAVEEN <bpkrips@gmail.co>
2021-03-08 12:19:05 +0530

    dd

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Home from './components/Home/Home'
-import Buying from './components/Buying/Buying'
-// import Routing from './components/Routing/Routing]/'
-import Routing from './components/Routing/Routing'
-ReactDOM.render( <React.StrictMode>  <Routing/></React.StrictMode> 
-              ,document.getElementById('root'))
